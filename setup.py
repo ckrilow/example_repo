c02caf9ede7bf3228f51394539220d4a6a228df0
ckrilow <chadkrilow@gmail.com>
2024-01-23 15:18:36 -0500

    Update to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='example_repo',
     version='0.1',
-    packages=find_packages,
-    install_requires=True,
+    packages=find_packages(),
+    #install_requires=True,
 )
 
