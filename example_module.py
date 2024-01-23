6b2d067577cd8721db82081678ea35b4af7ced7f
ckrilow <chadkrilow@gmail.com>
2024-01-23 14:35:13 -0500

    Initial commit

---
+++ example_module.py
@@ -0,0 +1,5 @@
+# example_repo/example_module.py
+
+def example_function():
+    return "Hello from the example module!"
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+# example_repo/setup.py
+from setuptools import setup
+
+setup(
+    name='example-repo',
+    version='0.1',
+    packages=['example_repo'],
+    install_requires=[
+        # List any dependencies here
+    ],
+)
+
